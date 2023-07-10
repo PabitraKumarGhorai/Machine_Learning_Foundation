02a7e7e1fe528020b482d55ef1c6e6c1bfe8d013
Pabitra Kumar Ghorai <ghoraipabitrakumar@gmail.com>
2023-07-10 22:52:28 +0530

    Updated EDA problem statement, Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 import _distutils_hack
-
+from src.logger import logging
 def error_msg_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename 
@@ -20,13 +20,13 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-#    try:
+    try:
         a=1/0
-#    except Exception as e:
-#        logging.info("Divide by Zero error !! ")
-#        raise CustomException(e,sys)
+    except Exception as e:
+        logging.info("Divide by Zero error !! ")
+        raise CustomException(e,sys)
         
         
 
